3d9d3e7a09d4d8d0a98f58fb44a2c33872764e0d
Nathalie Vrecq <nathalie.vrecq@gmail.com>
2025-03-14 15:05:02 +0100

    01 - first text display project.

---
+++ 01-premierProjetAffichacheTexte/src/Principale.java
@@ -0,0 +1,8 @@
+/*
+ * Un commentaire sur plusieurs lignes */
+public class Principale { // Ne pas modifier cette ligne
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+	}
+}
