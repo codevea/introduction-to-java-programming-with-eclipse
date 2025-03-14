6b61a3e90628fe00cc037ea1c13f0aa4a17d3e91
Nathalie Vrecq <nathalie.vrecq@gmail.com>
2025-03-14 15:19:25 +0100

    09 - void method.

---
+++ 09-methodeVoid/src/Principale09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Principale09 {
+
+	static void calculeAffichePrixSolde(double prixArticle, double tauxSolde) {
+		double prixSolde;
+		prixSolde = (1 - tauxSolde / 100) * prixArticle;
+		System.out.print("Le prix de l'article soldé est " + prixSolde + " €.");
+	}
+
+	public static void main(String[] args) {
+
+		double prixArticle = 0., tauxSolde = 0;
+
+		Scanner clavier = new Scanner(System.in);
+
+		System.out.println("Pour saisir un décimal au clavier, on utilise la virgule.");
+		System.out.print("Entrer le prix de l'article non soldé : ");
+		prixArticle = clavier.nextDouble();
+		System.out.print("Entrer le pourcentage de réduction : ");
+		tauxSolde = clavier.nextDouble();
+		clavier.close();
+
+		calculeAffichePrixSolde(prixArticle, tauxSolde);
+	}
+}
